fd66f7968641d58c77fb6b1c302a2a79055fb905
jsiang1 <100522161+jsiang1@users.noreply.github.com>
2023-04-30 15:12:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "rental.cmpj3qf36b1d.us-east-1.rds.amazonaws.com"
+customhost = "rental.cd78najmcvqj.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "abcd1234"
 customdb = "rental"
-custombucket = "pangjinsiang-rental"
+custombucket = "jinsiang-rental"
 customregion = "us-east-1"
 
